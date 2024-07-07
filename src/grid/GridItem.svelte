2229d194505f3ab6aecec39bee05bfe872da03cb
unit-404 <conway24b@gmail.com>
2024-07-08 00:09:18 +0700

    when mount reserve

--- src/grid/GridItem.svelte
+++ src/grid/GridItem.svelte
@@ -1,8 +1,14 @@
 <script>
+    import {onMount} from "svelte";
+    
+    //
     export let cell = [0, 0];
     export let id = "";
     export let label = "";
     export let pointerId = -1;
+
+    //
+    export let type = "";
 
     //
     let target = null;
@@ -10,9 +16,15 @@
     //
     $: cell?.["@subscribe"]?.((v)=>(target?.style?.setProperty?.("--cell-x", v, "")), 0);
     $: cell?.["@subscribe"]?.((v)=>(target?.style?.setProperty?.("--cell-y", v, "")), 1);
+    
+    //
+    export let whenMount = ()=>{};
+    
+    //
+    onMount(whenMount);
 </script>
 
 <!-- -->
-<div bind:this={target} data-id={id} class="ux-grid-item">
+<div bind:this={target} data-id={id} data-type={type} class="ux-grid-item">
     <slot></slot>
 </div>
--- src/grid/GridItemLabel.svelte
+++ src/grid/GridItemLabel.svelte
@@ -3,16 +3,25 @@
     export let id = "";
     export let label = "";
     export let pointerId = -1;
-
+    
+    //
+    export let type = "";
+    
     //
     let target = null;
 
     //
     $: cell?.["@subscribe"]?.((v)=>(target?.style?.setProperty?.("--cell-x", v, "")), 0);
     $: cell?.["@subscribe"]?.((v)=>(target?.style?.setProperty?.("--cell-y", v, "")), 1);
+    
+    //
+    export let whenMount = ()=>{};
+    
+    //
+    onMount(whenMount);
 </script>
 
 <!-- -->
-<div bind:this={target} data-id={id} class="ux-grid-item-label">
+<div bind:this={target} data-id={id} data-type={type} class="ux-grid-item-label">
     {label||""}
 </div>
