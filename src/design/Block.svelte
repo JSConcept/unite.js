f8a89627f2f4328d26c8673bb3a1cb06eaf3a173
unit-404 <conway24b@gmail.com>
2024-07-09 21:32:45 +0700

    block decor import

---
+++ src/design/Block.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+    import {propsFilter} from "../utils/Utils.ts";
+</script>
+
+<div class="block-decor solid" {...propsFilter($$props)}>
+    <div class="icon-wrap hl-1"  inert={true}>
+        <div class="icon-box" inert={true}><slot name="icon"></slot></div>
+    </div>
+    
+    <div class="label-box" inert={true}>
+        <slot></slot>
+    </div>
+    
+    <div class="in-element">
+        <slot name="element"></slot>
+    </div>
+
+    <!--<div class="description">
+    
+    </div>-->
+</div>
