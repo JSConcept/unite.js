da5fa9ae9542c02c3473a8fdc98ec2de75b6e4cb
unit-404 <conway24b@gmail.com>
2024-07-14 16:52:28 +0700

    WIP status bar

---
+++ appframe/StatusBar.svelte
@@ -0,0 +1,97 @@
+<script>
+import LucideIcon from "../design/WLucideIcon.svelte";
+
+//
+let signal = navigator.onLine ? (navigator?.connection?.effectiveType || "4g") : "offline";
+let battery = "battery-full";
+
+//
+const date = new Date();
+let timeMinutes = date.getMinutes();
+let timeHours = date.getUTCHours();
+
+//
+setInterval(()=>{
+    const date = new Date();
+    timeMinutes = date.getMinutes();
+    timeHours = date.getUTCHours();
+}, 500);
+
+//
+const changeSignal = ()=>{
+    if (navigator.onLine) {
+        signal = navigator?.connection?.effectiveType || "4g";
+    } else {
+        signal = "offline";
+    }
+}
+
+//
+const batteryIcons = new Map([
+    [0, "battery-warning"],
+    [25, "battery"],
+    [50, " battery-low"],
+    [75, "battery-medium"],
+    [100, "battery-full"],
+]);
+
+//
+const byLevel = (lv = 1.0)=>{
+    return batteryIcons.get(Math.round(lv / 0.25) * 25);
+}
+
+//
+const batteryStatus = navigator.getBattery?.();
+
+//
+const changeBatteryStatus = ()=>{
+    batteryStatus.then((btr)=>{
+        if (btr.charging) 
+            { battery = "battery-charging"; }  else
+            { battery = byLevel(btr.level); };
+    }).catch(console.warn.bind(console));
+}
+
+//
+batteryStatus.then((btr)=>{
+    btr.addEventListener("chargingchange", changeBatteryStatus);
+    btr.addEventListener("levelchange", changeBatteryStatus);
+});
+
+//
+navigator.connection?.addEventListener("change", changeSignal);
+
+//
+changeBatteryStatus();
+changeSignal();
+
+//
+const signalIcons = {
+    "offline": "wifi-off",
+    "4g": "wifi",
+    "3g": "wifi-high",
+    "2g": "wifi-low",
+    "slow-2g": "wifi-zero"
+}
+
+document.documentElement.addEventListener("contextmenu", (ev)=>{
+	const target = ev.target;
+	if ((target?.matches?.(".ux-status-bar") || target?.closest?.(".ux-status-bar"))) {
+		ev.stopPropagation();
+		ev.stopImmediatePropagation();
+		ev.preventDefault();
+	}
+}, {capture: true});
+
+</script>
+
+<!-- -->
+<div class="ux-status-bar ux-transparent">
+    <div class="left"></div>
+    <div class="center"></div>
+    <div class="right">
+        <div class="icon-wrap"><LucideIcon name={signalIcons[signal]||"wifi"}></LucideIcon></div>
+        <div class="icon-wrap"><LucideIcon name={battery || "battery-full"}></LucideIcon></div>
+        <div class="time"><span class="hours">{timeHours}</span>:<span class="minutes">{timeMinutes}</span></div>
+    </div>
+</div>
