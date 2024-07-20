bf55926576158a68da1951a57e38efa1dc938360
unit-404 <conway24b@gmail.com>
2024-07-21 03:27:43 +0700

    state manager

---
+++ scripts/reactive/StateManager.ts
@@ -0,0 +1,55 @@
+//
+export class StateManager {
+    elements = new WeakMap<HTMLElement, any>();
+    named = new Map<string, any>();
+
+    //
+    constructor() {
+        
+    }
+
+    //
+    setState(name: string, state: any, whatToDo: Function | null = null) {
+        this.named.set(name, state);
+        whatToDo?.(name, state);
+        return this;
+    }
+
+    //
+    bindState(element: HTMLElement, state: any, whatToDo: Function | null = null) {
+        this.elements.set(element, state);
+        whatToDo?.(element, state);
+        return this;
+    }
+
+    //
+    getState(element: HTMLElement) {
+        return element instanceof HTMLElement ? this.elements.get(element) : this.named.get(element);
+    }
+
+    //
+    stateBehave(element: HTMLElement | string, onChange: Function | null = null) {
+        // @ts-ignore
+        const state = (element instanceof HTMLElement ? this.elements : this.named).get(element);
+        
+        //
+        if (element instanceof HTMLElement) {
+            const ref = new WeakRef(element);
+            state?.["@subscribe"]?.((value, prop)=>{
+                onChange?.(ref.deref(), prop, value);
+            });
+        } else {
+            state?.["@subscribe"]?.((value, prop)=>{
+                onChange?.(element, prop, value);
+            });
+        }
+
+        return this;
+    }
+}
+
+//
+const states = new StateManager();
+
+//
+export default states;
