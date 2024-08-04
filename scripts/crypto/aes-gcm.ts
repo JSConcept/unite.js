c2b5c5c8900934e682c6d736e56c1510edd31136
unit-404 <conway24b@gmail.com>
2024-08-04 23:21:32 +0700

    crypto-api

--- scripts/crypto/aes-gcm.ts
+++ scripts/crypto/aes-gcm.ts
@@ -2,9 +2,14 @@
 const fromHexString = (hexString) => Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
 const toHexString = (bytes) => bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
 
+//
+const fromBase64String = (base64String)=> ( Uint8Array.from(window.atob(base64String), v => v.charCodeAt(0)) );
+const toBase64String = (bytes)=>(btoa(String.fromCharCode.apply(null, Array.from(bytes))));
+
+
 // BASE: Random Base64 from 16 bytes string as is
 export const useSecretBase64Key = (secretKeyBase64 = "")=>{
-    const key = Uint8Array.from(window.atob(secretKeyBase64), v => v.charCodeAt(0));
+    const key = fromBase64String(secretKeyBase64);
     if (key.length != 16) throw Error("Wrong key length (needs 16 bytes), has: " + key.length);
     return window.crypto.subtle.importKey(
         "raw",
@@ -15,7 +20,8 @@ export const useSecretBase64Key = (secretKeyBase64 = "")=>{
     );
 }
 
-//
+// IV not needed to send into server if you want store encrypted
+// used in client-side, used with secret key (encoded to OBJ with JS)
 export const encryptMessage = (keyObj, binary)=>{
     // iv will be needed for decryption
     const iv = window.crypto.getRandomValues(new Uint8Array(16));
@@ -23,14 +29,14 @@ export const encryptMessage = (keyObj, binary)=>{
         { name: "AES-GCM", iv },
         keyObj,
         binary,
-    ), toHexString(iv)];
+    ), toBase64String(iv)];
 }
 
-//
+// used in client-side when got data back (please, save IV and accessID for decrypt)
 export const decryptMessage = (keyObj, encryptedAndIV)=>{
     const [encrypted, iv] = encryptedAndIV;
     return window.crypto.subtle.decrypt(
-        { name: "AES-GCM", iv: fromHexString(iv) },
+        { name: "AES-GCM", iv: fromBase64String(iv) },
         keyObj,
         encrypted,
     );
@@ -42,10 +48,12 @@ export const generateSalt = ()=>{
 }
 
 // prefer use only in server-side
+// also, this key can be retrieved from server-side by 'accessKey'
+// but IV you needs to be saved
 export const makeSecretBase64Key = async (accessKey, salt = "")=>{
     const encoder = new TextEncoder();
     const hash = await window.crypto.subtle.digest("SHA-256", encoder.encode(accessKey + salt));
-    return btoa(String.fromCharCode.apply(null, Array.from(new Uint8Array(hash))));
+    return toBase64String(new Uint8Array(hash));
 }
 
 //
